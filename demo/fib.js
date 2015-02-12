08cc31d683692e05b4863a51e31904236b28fe25
garzon <610204679@qq.com>
2015-02-12 15:30:42 +0800

    add comment

--- demo/fib.js
+++ demo/fib.js
@@ -1,14 +1,13 @@
 function fib() {
 	function make_fib() {
 		function add(x, y) { return x+y; };
-		return Stream.cons(1, new Stream(function() {
-			return Stream.cons(1, new Stream(function() {
-				return Stream.merge(
-						new Stream(fib), 
-						new Stream(fib).cdr(),
-						add);
-			}));
-		}));
+		return Stream.cons(1, // fib[0] = 1
+				Stream.cons(1, // fib[1] = 1
+					new Stream(function() {  // lazy evaluation using "new Stream(function)"
+						return Stream.merge(fib(), fib().cdr(), add);  // fib[n+2] = fib[n] + fib[n+1]
+					})
+				)
+		);
 	}
 	var res = make_fib(); // save the result in res
 	fib = function() { return res; };
